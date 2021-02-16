9f0e7c82bf11185d148ee736e166e5b142826810
olli <olli.korhonen19@gmail.com>
2021-02-16 18:35:34 +0200

    create Crown component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCrown, faGem } from "@fortawesome/free-solid-svg-icons";
 
+import { Crown } from "./Crown";
+
 export function Card(props) {
 
   // Statuses
-  const bronze = { name: 'Bronze', color: '#cd7f32', margin: '10px' }
-  const silver = { name: 'Silver', color: '#C0C0C0', margin: '10px' }
-  const gold = { name: 'Gold', color: '#FFDF00', margin: '10px' }
-  const diamond = { name: 'diamond', color: '#66ccff', margin: '10px' }
+  const bronze = { name: 'Bronze', statusMark:'&#x2160', color: '#cd7f32', margin: '10px' }
+  const silver = { name: 'Silver', statusMark:'&#x2160', color: '#C0C0C0', margin: '10px' }
+  const gold = { name: 'Gold', statusMark:'&#x21604', color: '#FFDF00', margin: '10px' }
+  const diamond = { name: 'diamond', statusMark:'&#x2160', color: '#66ccff', margin: '10px' }
 
   // Check if index of card is even and set correct background.
   function background() {
@@ -96,105 +98,6 @@ export function Card(props) {
     }
   }
 
-
-  // Calculate past percentage of days in the correct statusarea
-  function percentage(status) {
-    let days = 0;
-    let fixedDays;
-    let percentage;
-
-    // If value is more than min, reduce min from value to get correct
-    // percentage of current statusarea. 
-    // example value is 100 days, min is 80 and max 120 in the current statusarea. 
-    // Value is more than days between min and max so percentage is over 100%.
-    if(status.max !== null) {
-      if (props.value > status.min) {
-        fixedDays = props.value - status.min;
-      } else {
-        fixedDays = props.value;
-      }
-      
-      // Calculate days amount between min and max
-      for (let i = status.min; i <= status.max; i++) {
-        days = days + 1;
-      }
-      
-      // Calculate past percentage of days
-      return percentage = fixedDays / days * 100;
-    }else{
-      return percentage = 100;
-    }
-  }
-
-  // Progressbar style
-  function progressBar(percent) {
-    return (
-      {
-        position: 'absolute',
-        zIndex: 10,
-        left: 0,
-        backgroundColor: 'green',
-        width: `${percent}%`,
-        maxWidth: '100%',
-        height: '10px',
-        borderRadius: 0
-      }
-    )
-  }
-
-  function progress() {
-    const status = checkStatus();
-    const percent = percentage(status);
-
-    switch (status.statusArea) {
-      case 1:
-        return (
-          <div>
-            <FontAwesomeIcon className="Icon" icon={faCrown} style={status.status} />
-            <p>{`${status.status.name} - `}&#8544;</p>
-            <div className="Progress-bar">
-              <span style={progressBar(percent)}></span>
-            </div>
-          </div>
-        )
-      case 2:
-        return (
-          <div>
-            <FontAwesomeIcon className="Icon" icon={faCrown} style={status.status} />
-            <FontAwesomeIcon className="Icon" icon={faCrown} style={status.status} />
-            <p>{`${status.status.name} - `}&#8545;</p>
-            <div className="Progress-bar">
-              <span style={progressBar(percent)}></span>
-            </div>
-          </div>
-        )
-      case 3:
-        return (
-          <div>
-            <FontAwesomeIcon className="Icon" icon={faCrown} style={status.status} />
-            <FontAwesomeIcon className="Icon" icon={faCrown} style={status.status} />
-            <FontAwesomeIcon className="Icon" icon={faCrown} style={status.status} />
-            <p>{`${status.status.name} - `}&#8546;</p>
-            <div className="Progress-bar">
-              <span style={progressBar(percent)}></span>
-            </div>
-          </div>
-        )
-      case 4:
-        return (
-          <div>
-            <FontAwesomeIcon className="Icon" icon={faGem} style={status.status} />
-            <p>{`${status.status.name}`}</p>
-            <div className="Progress-bar">
-              <span style={progressBar(percent)}></span>
-            </div>
-          </div>
-        )
-      default:
-        break;
-    }
-  }
-
   function update() {
     if(props.updated === false) {
       props.update();
@@ -205,7 +108,7 @@ export function Card(props) {
     <div onClick={() => update()} className="Card" style={background()}>
       <span>Started {props.startDate} - {props.value} days</span>
       <h3 className="Card-title">{props.title}</h3>
-      {progress()}
+      <Crown status={checkStatus()} value={props.value} />
     </div>
   );
 };
---
+++ src/components/Crown.js
@@ -0,0 +1,76 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCrown, faGem } from "@fortawesome/free-solid-svg-icons";
+
+export function Crown(props) {
+    const status = props.status;
+    const percent = percentage();
+    // Calculate past percentage of days in the correct statusarea
+    function percentage() {
+        let days = 0;
+        let fixedDays;
+        let percentage;
+
+        // If value is more than min, reduce min from value to get correct
+        // percentage of current statusarea. 
+        // example value is 100 days, min is 80 and max 120 in the current statusarea. 
+        // Value is more than days between min and max so percentage is over 100%.
+        if (status.max !== null) {
+            if (props.value > status.min) {
+                fixedDays = props.value - status.min;
+            } else {
+                fixedDays = props.value;
+            }
+
+            // Calculate days amount between min and max
+            for (let i = status.min; i <= status.max; i++) {
+                days = days + 1;
+            }
+
+            // Calculate past percentage of days
+            return percentage = fixedDays / days * 100;
+        } else {
+            return percentage = 100;
+        }
+    }
+
+    // Progressbar style
+    function progressBar(percent) {
+        return (
+            {
+                position: 'absolute',
+                zIndex: 10,
+                left: 0,
+                backgroundColor: 'green',
+                width: `${percent}%`,
+                maxWidth: '100%',
+                height: '10px',
+                borderRadius: 0
+            }
+        )
+    }
+
+    function setIcons() {
+        let icons = [];
+        if (status.statusArea !== 4) {
+            const crown = <FontAwesomeIcon className="Icon" icon={faCrown} style={status.status} />
+            for (let index = 0; index < status.statusArea; index++) {
+                icons.push(crown);
+            }
+        } else {
+            icons.push(<FontAwesomeIcon className="Icon" icon={faGem} style={status.status} />);
+        }
+        return icons;
+    }
+
+    return (
+        <div>
+            {setIcons().map((item, index) => (
+                <span key={index}>{item}</span>
+            ))}
+            <p>{`${status.status.name} - ${status.statusArea}`}</p>
+            <div className="Progress-bar">
+                <span style={progressBar(percent)}></span>
+            </div>
+        </div>
+    )
+};
